acf193351ea895b5318e743ed6f37d404b058dc8
alphatech774623 <princemaurya7746@gmail.com>
2025-07-07 00:36:12 +0530

    imported cors packege in server.js file

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ import express from 'express'
 import { connectDB } from './config/db.js';
 import routes from './routes/routes.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const port = process.env.PORT;
 const app = express();
 
-
+app.use(cors({
+  origin: process.env.CLIENT_URL, // Replace with your frontend URL
+  credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}))
